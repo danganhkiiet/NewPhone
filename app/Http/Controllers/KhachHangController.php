abc3070e75d48132a45637abca88eef28896d122
danganhkiiet <danganhkiiet@gmail.com>
2023-11-18 15:46:36 +0700

    add khach-hang them-xoa-sua

--- app/Http/Controllers/KhachHangController.php
+++ app/Http/Controllers/KhachHangController.php
@@ -3,12 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\KhachHang;
-class KhachHangController extends Controller
+use App\Models\khachhang;
+class khachhangController extends Controller
 {
     //
     public function danhSach(){
-        $lst_khachhang = KhachHang::all();
-        return view('khachhang/danh-sach', compact('lst_khachhang'));
+        $lst_khach_hang = khachhang::paginate(5);
+        if($ten = request()->ten)
+        {
+            $lst_khach_hang = khachhang::where('ten','like','%'.$ten)->paginate(5);
+        }
+        return view('khach-hang/danh-sach', compact('lst_khach_hang'));
+    }
+    public function themMoi(){
+        return view('khach_hang/them-moi');
+    }
+    public function xuLyThemMoi(Request $request){
+       $khach_hang = new khachhang();
+       $khach_hang->ten = $request->ten;
+       $khach_hang->email = $request->email;
+       $khach_hang->dia_chi = $request->dia_chi;
+       $khach_hang->so_dien_thoai = $request->so_dien_thoai;
+       $khach_hang->password = 1;
+       $khach_hang->save();
+       return redirect()->route('khach-hang.danh-sach')->with('thong_bao','Thêm mới thành công');
+    }
+    
+    public function capNhat($id){
+        $khach_hang = khachhang::find($id);
+        return view('khach-hang/cap-nhat', compact('khach_hang'));
+    }
+    
+    public function xuLyCapNhat(Request $request, $id){
+        $khach_hang = khachhang::find($id);
+        $khach_hang->ten = $request->ten;
+        $khach_hang->email = $request->email;
+        $khach_hang->dia_chi = $request->dia_chi;
+        $khach_hang->so_dien_thoai = $request->so_dien_thoai;
+        $khach_hang->password = 1;
+        $khach_hang->save();
+        return redirect()->route('khach-hang.danh-sach')->with('thong_bao','Cập nhật thành công');
+    }
+     public function xoa($id){
+        $khach_hang = khachhang::find($id);
+        $khach_hang->delete();
+        return redirect()->route('khach-hang.danh-sach')->with('thong_bao','Xóa thành công');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ Route::get('/nha-cung-cap/danh-sach',[NhaCungCapController::class,'danhSach'])->
 
 //khachhang
 Route::get('/khach-hang/danh-sach',[KhachHangController::class,'danhSach'])->name('khach-hang.danh-sach');
+Route::get('/khach-hang/them-moi',[KhachHangController::class,'themMoi'])->name('khach-hang.them-moi');
+Route::post('/khach-hang/them-moi',[KhachHangController::class,'xuLyThemMoi'])->name('khach-hang.xu-ly-them-moi');
+Route::get('/khach-hang/cap-nhat{id}',[KhachHangController::class,'capNhat'])->name('khach-hang.cap-nhat');
+Route::post('/khach-hang/cap-nhat{id}',[KhachHangController::class,'xuLyCapNhat'])->name('khach-hang.xu-ly-cap-nhat');
+Route::get('/khach-hang/xoa{id}',[KhachHangController::class,'xoa'])->name('khach-hang.xoa');
