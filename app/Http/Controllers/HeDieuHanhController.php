b4765152aa6eef7cf77e7a59c2ad9d9705aa9c2e
TuanKhanh2k3 <0306211149@caothang.edu.vn>
2023-11-19 10:13:52 +0700

    Add DoPhanGiai DungLuongRam va HeDieuHanh

---
+++ app/Http/Controllers/HeDieuHanhController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\HeDieuHanh;
+class HeDieuHanhController extends Controller
+{
+    public function danhSach(){
+        $lst_hedieuhanh=HeDieuHanh::paginate(5);
+
+        if($ten = request()->ten)
+        {
+            $lst_hedieuhanh=HeDieuHanh::where('ten','like','%'.$ten)->paginate(5);
+        }
+        return view('he-dieu-hanh.danh-sach',compact('lst_hedieuhanh'));
+    }
+    public function themMoi(){
+        return view('he-dieu-hanh.them-moi');
+    }
+    public function xuLyThemMoi(Request $request){
+        $request->validate([
+            'ten' => 'required',
+        ]);
+        $he_dieu_hanh=HeDieuHanh::create(['ten'=>$request->ten]);
+
+        return redirect()->route('he-dieu-hanh.danh-sach')->with('thong_bao','Thêm mới thành công');
+    }
+    public function capNhat($id){
+        $he_dieu_hanh=HeDieuHanh::find($id);
+        return view('he-dieu-hanh.cap-nhat',compact('he_dieu_hanh'));
+    }
+    
+    public function xuLyCapNhat(Request $request, $id){
+        $request->validate([
+            'ten' => 'required',
+        ]);
+        $he_dieu_hanh=HeDieuHanh::where('id',$request->id)->update(['ten'=>$request->ten]);
+
+        return redirect()->route('he-dieu-hanh.danh-sach')->with('thong_bao','Cập nhật thành công');
+    }
+    public function xoa($id){
+        $he_dieu_hanh=HeDieuHanh::find($id);
+       
+        if(empty($he_dieu_hanh)){
+            return "xóa thất bại";
+        }
+        $he_dieu_hanh->delete();
+        return redirect()->route('he-dieu-hanh.danh-sach')->with('thong_bao','Xóa thành công');
+    }
+}
--- app/Models/DungLuongRam.php
+++ app/Models/DungLuongRam.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class DungLuongRam extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $fillable = ['ten'];
     protected $table="dung_luong_ram";
 }
